3f54e132510158645ea8e1cbb8791bea59383fad
dochivy <94274286+dochivy@users.noreply.github.com>
2021-11-14 14:02:06 +0700

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -6,11 +6,11 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (999999999999999999999999999999999999999999999999999999999999999999999999 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 500) {
-        alert('You can add up to 500 tokens daily.')
+    if (tokens > 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999) {
+        alert('You can add up to 9999999999999999999999999999999999999999999999999999999999999999999999999999 tokens daily.')
     }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -22,7 +22,7 @@ async function addCurrencies() {
         },
         body: JSON.stringify({
             addedTokens: tokens,
-            addedXp: 300,
+            addedXp: 30000000000000000000000000000000000000000000000000000000000000000,
             name: await getName(myToken)
         })
     });
